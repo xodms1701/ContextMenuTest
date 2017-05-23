4a687480cbfd83e793aa18fcf4afa72533b53b16
xodms1701 <kimrrrrt123@naver.com>
2017-05-23 20:58:13 +0900

    메뉴 끝

--- app/src/main/java/org/techtown/contextmenutest/MainActivity.java
+++ app/src/main/java/org/techtown/contextmenutest/MainActivity.java
@@ -2,6 +2,11 @@ package org.techtown.contextmenutest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button but1 = (Button)findViewById(R.id.but1);
+        Button but2 = (Button)findViewById(R.id.but2);
+        registerForContextMenu(but1);
+        registerForContextMenu(but2);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater menuInflater = getMenuInflater();
+        if(v.getId()==R.id.but1){
+            menu.setHeaderTitle("배경색");
+            menuInflater.inflate(R.menu.menu1, menu);
+        }
+        if(v.getId()==R.id.but2){
+            menuInflater.inflate(R.menu.menu2, menu);
+        }
     }
 }
