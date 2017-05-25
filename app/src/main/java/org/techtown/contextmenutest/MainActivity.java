c655e48a277df12d3a60e40cf5e19930f10394af
xodms1701 <kimrrrrt123@naver.com>
2017-05-25 19:21:10 +0900

    done

--- app/src/main/java/org/techtown/contextmenutest/MainActivity.java
+++ app/src/main/java/org/techtown/contextmenutest/MainActivity.java
@@ -1,21 +1,29 @@
 package org.techtown.contextmenutest;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    LinearLayout Linear;
+    Button but2;
+    ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button but1 = (Button)findViewById(R.id.but1);
-        Button but2 = (Button)findViewById(R.id.but2);
+        Linear = (LinearLayout)findViewById(R.id.Linear);
+        but2 = (Button)findViewById(R.id.but2);
+        img = (ImageView)findViewById(R.id.img);
         registerForContextMenu(but1);
         registerForContextMenu(but2);
     }
@@ -31,5 +39,31 @@ public class MainActivity extends AppCompatActivity {
         if(v.getId()==R.id.but2){
             menuInflater.inflate(R.menu.menu2, menu);
         }
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        super.onContextItemSelected(item);
+        switch (item.getItemId()){
+            case R.id.menu1_item_blue:
+                Linear.setBackgroundColor(Color.BLUE);
+                return true;
+            case R.id.menu1_item_red:
+                Linear.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.menu1_item_pink:
+                Linear.setBackgroundColor(Color.rgb(255,178,217));
+                return true;
+            case R.id.menu2_item_rotation:
+                img.setRotation(img.getRotation()+90);
+                return true;
+            case R.id.menu2_item_size:
+                img.setScaleX(img.getScaleX()*1.5f);
+                img.setScaleY(img.getScaleY()*1.5f);
+                return true;
+
+        }
+
+        return false;
     }
 }
